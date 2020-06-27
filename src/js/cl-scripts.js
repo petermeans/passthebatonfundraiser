b66a3988e786da9f3c7b3d021ec488f52d122518
Peter Means <petermeans@Peters-MacBook-Air.local>
2020-06-27 10:47:58 -0700

    setup google api on index page

--- src/js/cl-scripts.js
+++ src/js/cl-scripts.js
@@ -1,58 +1,117 @@
-//https://docs.google.com/spreadsheets/d/1bwpJ0jaDZ7vUme9k7SRWc994NmqzLOGdFVcpXtPxPE8/edit?usp=sharing
-
 const googleSheetsModule = (function () {
     const s = {
-        sheetId: '12355',
-        gapiLoadedPromise:null,
-        loading:true
+        sheetId: '1i1C9cPeEW9pKDDV42THQVm2GAE2TX6ReHuiPlTMEDQ8',
+        sheetRange: 'ResponsesSheet1!C1:D',
+        gapiLoadedPromise: null,
+        loading: true,
+        dom:{
+            $nameBox: $('[data-ui="name-box"]'),
+            $contentBox: $('[data-ui="content-box"]'),
+            $spinnerBox:$('[data-ui="spinner-box"]')
+        }
+    }
+
+    const state = {
+        sheetData: {
+            results: 'not sent',
+            raw: null,
+            nice: null
+        }
     }
 
     const init = function () {
-        console.log('init - go')
+        console.log('init - go 2')
         s.gapiLoadedPromise = waitForGAPI();
-        s.gapiLoadedPromise.then(()=>{},()=>{
+        s.gapiLoadedPromise.then(() => { }, () => {
             console.log('Error: failed to load gapi')
         })
     }
 
-    const waitForGAPI = function (callback){
-        return new Promise((resolve,reject)=>{
+    const waitForGAPI = function (callback) {
+        return new Promise((resolve, reject) => {
             let count = 0
-            const myInterval = setInterval(()=>{
-                if(gapiReady){
+            const myInterval = setInterval(() => {
+                if (gapiReady) {
                     clearInterval(myInterval)
                     resolve()
                 }
-                else{
+                else {
                     count++
-                    if(count > 300){
+                    if (count > 300) {
                         clearInterval(myInterval)
                         reject();
                     }
-                    //asdf
                 }
-            },100)
+            }, 100)
         })
     }
 
     const getSheetData = function () {
-        gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
-            range: "Class Data!A2:E",
-        }).then(
-            function(response){
-                console.log('got somethin!')
-                console.log(response)
-            }
-        )
-    }
-
-    const getLatestRunner = function(){
-        console.log('getLatestRunner...')
-        s.gapiLoadedPromise.then(()=>{
-            getSheetData()
+        return new Promise((resolve, reject) => {
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: s.sheetId,
+                range: s.sheetRange
+            }).then(
+                (response) => {
+                    var rows = response.result.values;
+                    if (rows.length > 0) {
+                        state.sheetData.raw = rows;
+                        state.sheetData.nice = formatData(rows)
+                        state.sheetData.results = 'success - data found'
+                    } else {
+                        state.sheetData.results = 'no data found'
+                        console.log("no data found");
+                    }
+                    resolve();
+                }
+            )
         })
     }
+
+    const formatData = function (rows) {
+        return rows.map((row, i) => {
+            return { name: row[0], comment: row[1] }
+        }).filter((obj, i) => {
+            return i > 0 //getting rid of the field names
+        })
+    }
+
+    const getLatestRunner = function () {
+        console.log('getLatestRunner 5 ...')
+        s.gapiLoadedPromise.then(() => {
+            getSheetData().then(() => {
+                const latestRunner = state.sheetData.nice[state.sheetData.nice.length - 1]
+                s.dom.$nameBox.text(latestRunner.name)
+                hideBox(s.dom.$spinnerBox)
+                showBox(s.dom.$contentBox)
+            });
+        })
+    }
+
+    const hideBox = function($box){
+        $box.addClass('d-none')
+    }
+
+    const showBox = function($box){
+        console.log('show box....')
+        console.log($box)
+        $box.removeClass('d-none')
+    }
+
+    return {
+        s,
+        state,
+        init,
+        getSheetData,
+        getLatestRunner
+    }
+})();
+
+
+
+
+
+
 
 
     // function listMajors() {
@@ -80,16 +139,3 @@ const googleSheetsModule = (function () {
     //             }
     //         );
     // }
-    
-
-    return {
-        s,
-        init,
-        getSheetData,
-        getLatestRunner
-    }
-})();
-
-
-
-
